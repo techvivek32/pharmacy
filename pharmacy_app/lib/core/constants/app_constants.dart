cd77d624308ec456edc5a50615d02b639051c5d4
mjrajput04 <manojvagadiya04@gmail.com>
2026-03-28 18:50:56 +0530

    pharmacy_app env addd

--- pharmacy_app/lib/core/constants/app_constants.dart
+++ pharmacy_app/lib/core/constants/app_constants.dart
@@ -1,8 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppConstants {
-  static const String baseUrl = 'https://medi-express-vivekvora3226-2527s-projects.vercel.app/api';
-  
+  static String get baseUrl =>
+      dotenv.env['API_BASE_URL'] ?? 'https://pharmacy-five-eosin.vercel.app/api';
+
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
-  
+
   static const Duration connectionTimeout = Duration(seconds: 30);
 }
--- pharmacy_app/lib/main.dart
+++ pharmacy_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/theme/app_theme.dart';
 import 'providers/auth_provider.dart';
@@ -10,7 +11,9 @@ import 'features/home/screens/home_screen.dart';
 import 'features/requests/screens/prescription_detail_screen.dart';
 import 'features/requests/screens/quote_builder_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
